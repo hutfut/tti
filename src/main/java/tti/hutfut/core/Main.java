f34a903ccc1f6c77a79cde8e8790acb00c2c66cd
hutfut <hutfut@yahoo.com>
2015-03-21 17:29:39 -0400

    initial commit

---
+++ src/main/java/tti/hutfut/core/Main.java
@@ -0,0 +1,23 @@
+package tti.hutfut.core;
+
+/* 
+ * Main class to launch embedded Jetty server
+ * 
+ * embedded jetty and spring MVC framework thanks to github.com/orb15
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try {
+			JettyConfig jettyConfig = new JettyConfig();
+			EmbeddedJetty.startServer(jettyConfig);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
